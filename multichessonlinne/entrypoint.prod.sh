b27d2b1000c636a4bc249f139147e9e66167b9db
Max <102045786+Thereistance@users.noreply.github.com>
2025-05-23 12:42:29 +0300

    commit

--- multichessonlinne/entrypoint.prod.sh
+++ multichessonlinne/entrypoint.prod.sh
@@ -1,7 +1,13 @@
-
 #!/bin/sh
 
 set -e
+
+# Ждем готовности PostgreSQL (добавьте эти строки)
+echo "Waiting for PostgreSQL..."
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+echo "PostgreSQL started"
 
 python manage.py migrate
 python manage.py collectstatic --noinput
