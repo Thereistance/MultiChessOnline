69c0c17e0878a6448c730e93b198e206067a79df
Max <102045786+Thereistance@users.noreply.github.com>
2025-05-23 13:03:27 +0300

    commit

--- multichessonlinne/entrypoint.prod.sh
+++ multichessonlinne/entrypoint.prod.sh
@@ -3,14 +3,12 @@
 set -e
 
 echo "Waiting for PostgreSQL..."
-python manage.py check --database default > /dev/null 2>&1
-while [ $? -ne 0 ]; do
+until pg_isready -h db -U postgres; do
   sleep 1
-  python manage.py check --database default > /dev/null 2>&1
 done
 echo "PostgreSQL is up - continuing"
 
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-exec gunicorn multichessonlinne.wsgi:application --bind 0.0.0.0:8000
+exec daphne -b 0.0.0.0 -p 8000 multichessonlinne.asgi:application
